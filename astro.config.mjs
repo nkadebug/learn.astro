e4957e84ddced06dab5f6afb5ee8d7226982a664
nkadebug <nka.debug@gmail.com>
2023-05-12 05:51:49 +0000

    astro add prefetch

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
-
 import svelte from "@astrojs/svelte";
+
+import prefetch from "@astrojs/prefetch";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://nkadebug-learn-astro.pages.dev/',
-  integrations: [tailwind(), sitemap(), svelte()]
+  integrations: [tailwind(), sitemap(), svelte(), prefetch()]
 });
